0e57e3ec5f616138bb9ef705f4f49a876fe39eef
joinThe <joinThe@users.noreply.github.com>
2025-01-08 04:25:50 +0000

    deploy: 8dc5f9fe69939cf6d885e8218047820f3d0cc890

--- assets/index-U-UUcR1c.js
+++
@@ -1,1 +0,0 @@
-import{j as r,M as t,T as a}from"./index-D4NJb-dh.js";function s(){return r.jsx(t,{title:"Головна сторінка",children:r.jsx(a,{children:"⬅ Виберіть сторінку"})})}export{s as default};

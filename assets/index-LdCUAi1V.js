4156e012f3dc1fe789df9f45efa5424c262d0b46
joinThe <joinThe@users.noreply.github.com>
2025-01-22 20:23:41 +0000

    deploy: 8f30f36facf04ece3a8609e85bfca0cc6e7545a7

--- assets/index-LdCUAi1V.js
+++
@@ -1,1 +0,0 @@
-import{j as r,M as t,T as a}from"./index-CAG9B7ok.js";function s(){return r.jsx(t,{title:"Головна сторінка",children:r.jsx(a,{children:"⬅ Виберіть сторінку"})})}export{s as default};

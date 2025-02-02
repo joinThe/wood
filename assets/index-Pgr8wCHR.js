308f16059c14bae4d5f4ee72922060fbedf3d8c3
joinThe <joinThe@users.noreply.github.com>
2025-02-02 02:36:05 +0000

    deploy: ccb49133d3d91929da1bd48e98b8a5c56928591d

--- assets/index-Pgr8wCHR.js
+++
@@ -1,1 +0,0 @@
-import{j as r,M as t,T as a}from"./index-B_BiDdmU.js";function s(){return r.jsx(t,{title:"Головна сторінка",children:r.jsx(a,{children:"⬅ Виберіть сторінку"})})}export{s as default};
---
+++ assets/index-d370_2xk.js
@@ -0,0 +1,1 @@
+import{j as r,M as t,T as a}from"./index-CwwjDLM8.js";function s(){return r.jsx(t,{title:"Головна сторінка",children:r.jsx(a,{children:"⬅ Виберіть сторінку"})})}export{s as default};
